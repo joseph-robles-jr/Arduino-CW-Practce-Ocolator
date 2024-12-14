8039484c794f0a11e5c4b2d84db04df881b0789f
Joseph Robles Jr. <156927934+joseph-robles-jr@users.noreply.github.com>
2024-12-13 20:04:40 -0700

    added potentiometer code. Updated readme.md

--- arduino_paddle_CW.ino
+++ arduino_paddle_CW.ino
@@ -2,6 +2,7 @@
 #define DAH_PIN 3
 #define OUTPUT_PIN 13 // Built-in LED pin on most Arduino boards
 #define SPEAKER_PIN 9
+#define POT_PIN A0 // Potentiometer connected to analog pin A0
 
 int ditState = 0;
 int dahState = 0;
@@ -9,9 +10,9 @@ int lastDitState = 0;
 int lastDahState = 0;
 bool isSending = false;
 
-unsigned long ditTime = 100; // Duration of a dit in milliseconds
-unsigned long dahTime = 3 * ditTime; // Duration of a dah in milliseconds
-unsigned long pauseTime = ditTime; // Duration of a pause between elements
+unsigned long ditTime = 100; // Initial duration of a dit in milliseconds
+unsigned long dahTime = 3 * ditTime; // Initial duration of a dah in milliseconds
+unsigned long pauseTime = ditTime; // Initial duration of a pause between elements
 unsigned int toneFrequency = 1000; // Frequency of the tone in Hertz (1 kHz)
 
 void setup() {
@@ -23,6 +24,12 @@ void setup() {
 }
 
 void loop() {
+  // Read the potentiometer value and map it to a suitable range for ditTime
+  int potValue = analogRead(POT_PIN);
+  ditTime = map(potValue, 0, 1023, 50, 500); // Map pot value to range 50ms - 500ms
+  dahTime = 3 * ditTime; // Dah time is three times the dit time
+  pauseTime = ditTime; // Pause time is equal to the dit time
+  
   ditState = digitalRead(DIT_PIN);
   dahState = digitalRead(DAH_PIN);
   
